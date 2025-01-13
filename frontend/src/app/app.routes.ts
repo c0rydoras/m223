086bacc7dff86479fa45424631e9aed259852167
Arthur <110528300+c0rydoras@users.noreply.github.com>
2025-01-13 16:09:13 +0100

    frontend: booking (#1)

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,11 +2,17 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { LedgerComponent } from './components/ledger/ledger.component';
 import { AuthGuard } from './guards/auth.guard';
+import { BookingComponent } from './components/booking/booking.component';
 
 export const routes: Routes = [
     {
         path: 'ledgers',
         component: LedgerComponent,
+        canActivate: [AuthGuard],
+    },
+    {
+        path: 'booking',
+        component: BookingComponent,
         canActivate: [AuthGuard],
     },
     {
--- frontend/src/app/components/booking/booking.component.ts
+++ frontend/src/app/components/booking/booking.component.ts
@@ -1,9 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LedgerService } from '../../services/ledger.service';
+import { HttpClient } from '@angular/common/http';
+import { Ledger } from '../../models/ledger.interface';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { BookingService } from '../../services/booking.service';
 
 @Component({
     selector: 'app-booking',
-    imports: [],
+    imports: [CommonModule, FormsModule],
     templateUrl: './booking.component.html',
     styleUrl: './booking.component.css',
+    providers: [LedgerService, BookingService, HttpClient],
 })
-export class BookingComponent {}
+export class BookingComponent implements OnInit {
+    ledgers: Ledger[] = [];
+
+    to: string | null = null;
+    from: string | null = null;
+    amount: string | null = null;
+    errorMessage = '';
+    loading = false;
+
+    constructor(
+        private ledgerService: LedgerService,
+        private bookingService: BookingService,
+    ) {}
+
+    ngOnInit(): void {
+        this.loadLedgers();
+    }
+
+    loadLedgers(): void {
+        this.ledgerService.getLedgers().subscribe({
+            next: (data: Ledger[]) => {
+                this.ledgers = data;
+            },
+            error: (error) => {
+                console.error('Error fetching ledgers', error);
+            },
+        });
+    }
+
+    get fromOptions() {
+        return this.ledgers.filter((l) => l.id !== Number.parseInt(this.to ?? '-1'));
+    }
+
+    get toOptions() {
+        return this.ledgers.filter((l) => l.id !== Number.parseInt(this.from ?? '-1'));
+    }
+
+    get isValid() {
+        return this.amount && this.from && this.to;
+    }
+
+    onSubmit(): void {
+        if (!(this.amount && this.from && this.to)) return;
+        this.errorMessage = '';
+        this.loading = true;
+        this.bookingService.makeBooking(Number.parseInt(this.from), Number.parseInt(this.to), Number.parseInt(this.amount)).subscribe({
+            next: () => {
+                this.loadLedgers();
+                this.loading = false;
+            },
+            error: (error) => {
+                this.errorMessage = `Transfer failed: ${error.error}`;
+                this.loading = false;
+            },
+        });
+    }
+}
