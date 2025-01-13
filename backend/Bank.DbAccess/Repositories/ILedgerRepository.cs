85febc2c40fe77fa2b7b1355174d91ef8a21aba4
e544631 <andrin.gurtner2@sbb.ch>
2025-01-13 08:19:55 +0100

    add ef core

--- backend/Bank.DbAccess/Repositories/ILedgerRepository.cs
+++ backend/Bank.DbAccess/Repositories/ILedgerRepository.cs
@@ -9,8 +9,5 @@ public interface ILedgerRepository
     public string Book(decimal amount, Ledger from, Ledger to);
     decimal GetTotalMoney();
     Ledger? SelectOne(int id);
-    Ledger? SelectOne(int id, MySqlConnection conn, MySqlTransaction? transaction);
-    void Update(Ledger ledger, MySqlConnection conn, MySqlTransaction transaction);
     void Update(Ledger ledger);
-    decimal? GetBalance(int ledgerId, MySqlConnection conn, MySqlTransaction transaction);
 }
