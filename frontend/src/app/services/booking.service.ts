394059a4367b347d37b7ccd1854aa7805d60f787
Jannik Pulfer <jpu140816@stud.gibb.ch>
2025-01-13 14:24:31 +0100

    Implement booking service and generate booking component

---
+++ frontend/src/app/services/booking.service.ts
@@ -0,0 +1,29 @@
+﻿import {Injectable} from '@angular/core';
+import {Observable, of} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+import {AuthService} from './auth.service';
+
+@Injectable({
+    providedIn: "root",
+})
+export class BookingService{
+    private apiUrl = "http://localhost:5000/api/v1";
+
+    constructor(
+        private http: HttpClient,
+        private authService: AuthService,
+    ) {}
+
+    makeBooking(fromLedgerId: number, toLedgerId: number, amount: number): Observable<unknown> {
+        const payload = {
+            fromLedgerId,
+            toLedgerId,
+            amount,
+        };
+        const token = this.authService.getToken();
+        if (token) {
+            return this.http.post(`${this.apiUrl}/ledgers/transfer`, payload);
+        }
+        return of(null);
+    }
+}
