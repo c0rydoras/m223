f5c635c097b8c0f6a246aa329186193979bb66e8
Jannik Pulfer <jpu140816@stud.gibb.ch>
2025-01-17 14:14:53 +0100

    Fix multi user test by using sqlite

--- backend/SingleUserTests/Startup.cs
+++ backend/SingleUserTests/Startup.cs
@@ -16,17 +16,9 @@ public class Startup
         services.AddTransient<ILoginService, LoginService>();
         services.AddTransient<IBookingRepository, BookingRepository>();
         services.AddTransient<IBookingService, BookingService>();
-        /*services.AddDbContext<AppDbContext>(options =>
+        services.AddDbContext<AppDbContext>(options =>
             options.UseSqlite(
                 "Filename=test",
-                sqlOptions => sqlOptions.MigrationsAssembly("Bank.Web")
-            )
-        );
-            */
-        services.AddDbContext<AppDbContext>(options =>
-            options.UseMySql(
-                "Server=localhost;Database=bank;User=bank_user;Password=bank_user_pw;",
-                new MySqlServerVersion(new Version(11, 6, 2)),
                 sqlOptions => sqlOptions.MigrationsAssembly("Bank.Web")
             )
         );
